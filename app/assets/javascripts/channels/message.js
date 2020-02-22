30fe49215523fcd257eb2886dca5e8b82a624a10
Nazo777 <7nazo-san7@ezweb.ne.jp>
2020-02-23 02:06:31 +0900

    SubmitEventFiring

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,3 +1,15 @@
 $(function(){
-  console.log('OK');
+  $('#new_message').on("submit",function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajex({
+      url: url,
+      type: 'POST',
+      data: formData,
+      datatype: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
