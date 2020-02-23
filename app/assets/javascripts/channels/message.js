40031cf7faea2be7a7b7589bbc40d102556333aa
Nazo777 <7nazo-san7@ezweb.ne.jp>
2020-02-24 02:38:16 +0900

    MessageImageView

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,9 +1,43 @@
 $(function(){
+
+      function buildHTML(message){
+        if ( message.image ) {
+          var html =
+          `<div class="main-message">
+          <div class="main-message__up-user">
+            <p class="main-message__up-user__up-name">
+              ${message.user_name}
+            </p>
+            <p class="main-message__up-user__up-day">
+              ${message.created_at}
+            </p>
+          </div>
+            <img class="main-message__image" src=${message.image}>
+        </div>`
+          return html;
+        } else {
+          var html =
+          `<div class="main-message">
+            <div class="main-message__up-user">
+              <p class="main-message__up-user__up-name">
+                ${message.user_name}
+              </p>
+              <p class="main-message__up-user__up-day">
+                ${message.created_at}
+              </p>
+            </div>
+              <p class="main-message__text">
+                ${message.text}
+              </p>
+          </div>`
+          return html;
+        };
+      }
   $('#new_message').on("submit",function(e){
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajaparamsx({
+    var url = $(this).attr('action');
+    $.ajax({
       url: url,
       type: 'POST',
       data: formData,
@@ -11,5 +45,11 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.main-messages').append(html);      
+        $('form')[0].reset();
+        console.log(this);
+      })
+  });
 });
