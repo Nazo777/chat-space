e7b28f055b1c2fdd3c3d3188f71232db05be7180
Nazo777 <7nazo-san7@ezweb.ne.jp>
2020-02-25 02:04:14 +0900

    ViewIncrementalSearch

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,44 @@
 $(function(){
 
+  function  appendUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>
+              `
+    $("#user-search-result").append(html)
+  }
+  function  appendErorr(){
+    var html = `
+               <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+               </div>`
+    $("#user-search-result").append(html)
+  }
+
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users" ,
+      data: { keyword: input },
+      dataType: "json"
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        appendErorr();
+      }
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
+    });
   })
 });
