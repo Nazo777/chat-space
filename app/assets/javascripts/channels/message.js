6d0e305441b8f38402c5c48368fbed3cb4bbbc5f
Nazo777 <7nazo-san7@ezweb.ne.jp>
2020-03-04 20:49:19 +0900

    CreateAutomaticUpdating

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -3,7 +3,7 @@ $(function(){
       function buildHTML(message){
         if ( message.image ) {
           var html =
-          `<div class="main-message">
+          `<div class="main-message" data-message-id="${message.id}">
             <div class="main-message__up-user">
               <p class="main-message__up-user__up-name">
                 ${message.user_name}
@@ -17,7 +17,7 @@ $(function(){
           return html;
         } else {
           var html =
-          `<div class="main-message">
+          `<div class="main-message" data-message-id="${message.id}">
             <div class="main-message__up-user">
               <p class="main-message__up-user__up-name">
                 ${message.user_name}
@@ -33,6 +33,7 @@ $(function(){
           return html;
         };
       }
+
   $('#new_message').on("submit",function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -55,4 +56,29 @@ $(function(){
         alert("メッセージ送信に失敗しました");
     });
   });
+  var reloadMessages = function(){
+    var last_message_id = $('.main-message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function (messages) { 
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+        });
+        $('.main-messages').append(insertHTML);
+        $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert("error");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
